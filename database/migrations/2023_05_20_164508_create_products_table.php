9d247bda0a8cc0f8250ac2ea5dc053615e6771ec
HalaFo <hfoaud74@gmail.com>
2023-06-01 23:18:13 +0300

    my changes

--- database/migrations/2023_05_20_164508_create_products_table.php
+++ database/migrations/2023_05_20_164508_create_products_table.php
@@ -32,3 +32,76 @@ class CreateProductsTable extends Migration
         Schema::dropIfExists('products');
     }
 }
+?>
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Category;
+use App\Models\Product;
+use Illuminate\Http\Request;
+
+class ProductController extends Controller
+{
+    //
+    public function index()
+    {
+        $products = Product::all();
+        return view('admin.products.index', compact('products'));
+    }
+
+    public function create()
+    {
+
+        $categories = Category::all();
+        return view('admin.products.create', compact('categories')); //بدي ارسلهم للفيو
+
+
+    }
+
+    public function store(Request $request)
+
+    {
+        $product = new Product();
+
+        $product->name = $request->name;
+        $product->quantity = $request->quantity;
+        $product->price = $request->price;
+        $product->category_id = $request->category_id;
+        $product->description = $request->description;
+
+        $product->save();
+
+        return redirect()->back();
+    }
+
+    public function show(Product $product)
+    {
+    }
+
+
+
+    public function edit($id)
+    {
+        $product = Product::find($id);
+        return view('admin.products.edit', compact('products'));
+    }
+
+
+    public function update(Request $request, $id)
+    {
+        $product = Product::find($id);
+        $product->name = $request->name;
+        $product->quantity = $request->quantity;
+        $product->price = $request->price;
+        $product->category_id = $request->category_id;
+        $product->description = $request->description;
+        $product->save();
+        return redirect('products');
+    }
+    public function destroy($id)
+    {
+        Product::find($id)->delete();
+        return redirect()->back();
+    }
+}
